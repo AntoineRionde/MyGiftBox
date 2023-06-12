89a1b976f0d61f161a16753c4d815908817c5f13
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2023-06-12 11:32:20 +0200

    Ajoute checkPassStrength

--- gift.appli/src/services/Auth/Auth.php
+++ gift.appli/src/services/Auth/Auth.php
@@ -15,10 +15,51 @@ class Auth
     }
 
     //Inscription
+    /**
+     * @param string $pass le mdp entré pas l'utilisateur
+     * @param int $min taille minimale du mdp
+     * @return bool True ou False si les condtions du mdp sont remplies ou pas
+     */
     private static function checkPassStrength(string $pass, int $min):bool {
-        $status = false;
 
-        return $status;
+        $errors = []; // Tableau pour stocker les messages d'erreur
+
+        // Vérifiez la force du mot de passe en utilisant des expressions régulières
+        $uppercase = preg_match('/[A-Z]/', $pass); // Vérifie la présence d'au moins une majuscule
+        $lowercase = preg_match('/[a-z]/', $pass); // Vérifie la présence d'au moins une minuscule
+        $number = preg_match('/[0-9]/', $pass); // Vérifie la présence d'au moins un chiffre
+        $specialChars = preg_match('/[^a-zA-Z0-9]/', $pass); // Vérifie la présence d'au moins un caractère spécial
+        $length = strlen($pass) >= 8; // Vérifie que le mot de passe a au moins 8 caractères de longueur
+
+        if (!$uppercase) {
+            $errors[] = "Le mot de passe doit contenir au moins une majuscule.";
+        }
+
+        if (!$lowercase) {
+            $errors[] = "Le mot de passe doit contenir au moins une minuscule.";
+        }
+
+        if (!$number) {
+            $errors[] = "Le mot de passe doit contenir au moins un chiffre.";
+        }
+
+        if (!$specialChars) {
+            $errors[] = "Le mot de passe doit contenir au moins un caractère spécial.";
+        }
+
+        if (!$length) {
+            $errors[] = "Le mot de passe doit avoir une longueur d'au moins 8 caractères.";
+        }
+
+        if (!empty($errors)) {
+            // Afficher tous les messages d'erreur
+            foreach ($errors as $error) {
+                echo $error;
+            }
+            return false;
+        }
+
+        return true;
     }
 
     public static function register(string $email, string $pass): void {
