c0a3c5b483f3de5dbfdfd5b9210d84ce61762833
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2023-06-13 15:36:36 +0200

    avancement classe Auth et User

--- gift.appli/src/models/User.php
+++ gift.appli/src/models/User.php
@@ -1,27 +1,34 @@
 <?php
 
 namespace gift\app\models;
+use Exception;
 use Illuminate\Database\Eloquent\Model;
+use netvod\exception\InvalidPropertyNameException;
 
 class User extends Model
 {
     protected $table = 'users'; // Nom de la table dans la base de données
 
-    protected $primaryKey = 'id';
+    protected $primaryKey = 'id'; //int(11) auto-increment
 
     public $timestamps = false;
 
     protected $fillable = [
-        'name',
-        'firstname',
-        'email',
-        'password'
+        'name',                 //varchar(256)
+        'firstname',            //varchar(256)
+        'email',                //varchar(256)
+        'password'              //varchar(256)
     ];
 
     protected $hidden = [
-        'password',
-        'activation_token',
-        'is_active'
+        'password',             //varchar(256)
+        'activation_token',     //varchar(128)
+        'activation_expires',   //timestamp
+        'renew_token',          //varchar(128)
+        'renew_expires',        //timestamp
+        'is_active',            //tinyint(4) [ 0 ]
+        'role',                 //int(4)
+        'level'                 //int(4)
     ];
 
     public function boxes()
@@ -29,14 +36,24 @@ class User extends Model
         return $this->hasMany(Box::class, 'box_id');
     }
 
-    public function setPassword(string $password): void
+    public function setPassword(string $pass)
     {
-        $this->attributes['password'] = password_hash($password, PASSWORD_DEFAULT);
+        $this->attributes['password'] = password_hash($pass, PASSWORD_DEFAULT);
     }
 
     public function activate(): void
     {
         $this->attributes['is_active'] = true;
         $this->attributes['activation_token'] = null;
+    }
+
+    public function hasRole(int $role): bool
+    {
+        return $this->attributes['role'] === $role;
+    }
+
+    public function hasLevel(int $level): bool
+    {
+        return $this->attributes['level'] >= $level;
     }
 }
