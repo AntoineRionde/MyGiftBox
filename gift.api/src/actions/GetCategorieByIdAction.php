651a4fc4fb7e93f622ce20decb1f6017f9ae0d3b
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2023-06-13 09:42:52 +0200

    maj User.php & création des classes d'actions API

---
+++ gift.api/src/actions/GetCategorieByIdAction.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace gift\api\actions;
+
+class GetCategorieByIdAction
+{
+
+}
--- gift.appli/src/models/User.php
+++ gift.appli/src/models/User.php
@@ -3,7 +3,40 @@
 namespace gift\app\models;
 use Illuminate\Database\Eloquent\Model;
 
-class User
+class User extends Model
 {
+    protected $table = 'users'; // Nom de la table dans la base de données
 
+    protected $primaryKey = 'id';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'name',
+        'firstname',
+        'email',
+        'password'
+    ];
+
+    protected $hidden = [
+        'password',
+        'activation_token',
+        'is_active'
+    ];
+
+    public function boxes()
+    {
+        return $this->hasMany(Box::class, 'box_id');
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->attributes['password'] = password_hash($password, PASSWORD_DEFAULT);
+    }
+
+    public function activate(): void
+    {
+        $this->attributes['is_active'] = true;
+        $this->attributes['activation_token'] = null;
+    }
 }
