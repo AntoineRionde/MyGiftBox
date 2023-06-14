421081f0fe650f28a01c5238d5c02d03fbb2aefc
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2023-06-14 08:40:40 +0200

    Modifie Users data

--- sql/creation_data_user.sql
+++ sql/creation_data_user.sql
@@ -7,4 +7,4 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 INSERT INTO `user` (`id`, `name`, `firstname`, `email`, `password`, `activation_token`, `activation_expire`, `renew_token`, `renew_expires`, `is_active`, `role`, `level`, `created_at`)
 VALUES
-    (1, 'admin', 'admin', 'bosstoinou@gmail.com', 'admin', 'fb5d3a14f5085b4decb07cff6f5b20b67fb2684136f92ef31e7fa4d9f3e0ddb2', '2023-06-13 16:20:00', '4f629e1ad04fb29476b5df0126f23469dd18c27d08745e1ef6743708628391f2ba83a224d601f983362bc492dee6ee8e87e39a79640a9d452e1eeac5f052f3ab', '2023-06-13 15:50:00', 1, 'SUPER_ADMIN', 1, '2023-06-13 14:41:00');
+    (1, 'admin', 'admin', 'Admin@mail.com', 'Admin1234_', 'fb5d3a14f5085b4decb07cff6f5b20b67fb2684136f92ef31e7fa4d9f3e0ddb2', '2023-06-16 16:20:00', '4f629e1ad04fb29476b5df0126f23469dd18c27d08745e1ef6743708628391f2ba83a224d601f983362bc492dee6ee8e87e39a79640a9d452e1eeac5f052f3ab', '2023-06-17 15:50:00', 1, 1000 , 1000, '2023-06-13 14:41:00');
--- sql/creation_schema_user.sql
+++ sql/creation_schema_user.sql
@@ -1,17 +1,17 @@
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
                         `id` int(11) NOT NULL AUTO_INCREMENT,
-                        `name` varchar(255) NOT NULL,
-                        `firstname` varchar(255) NOT NULL,
-                        `email` varchar(255) NOT NULL,
-                        `password` varchar(255) NOT NULL,
-                        `activation_token` varchar(255) NOT NULL,
+                        `name` varchar(256) NOT NULL,
+                        `firstname` varchar(256) NOT NULL,
+                        `email` varchar(256) NOT NULL,
+                        `password` varchar(256) NOT NULL,
+                        `activation_token` varchar(128) NOT NULL,
                         `activation_expire` timestamp,
                         `renew_token` varchar(128),
                         `renew_expires` timestamp,
-                        `is_active` tinyint(1) NOT NULL DEFAULT '0',
-                        `role` varchar(255) NOT NULL,
-                        `level` int(11) NOT NULL DEFAULT '1',
+                        `is_active` tinyint(4) NOT NULL DEFAULT '0',
+                        `role` int(4) NOT NULL,
+                        `level` int(4) NOT NULL DEFAULT '1',
                         `created_at` datetime NOT NULL,
                         PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
