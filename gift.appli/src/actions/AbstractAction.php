c7886d7145ef919e76561d2578f0302fdb2735b3
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2023-06-15 20:32:45 +0200

    css (on work)

--- gift.appli/src/actions/AbstractAction.php
+++ gift.appli/src/actions/AbstractAction.php
@@ -5,7 +5,7 @@ namespace gift\app\actions;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 
-abstract class AbstractAction {
+abstract class  AbstractAction {
 
     abstract public function __invoke(Request $request, Response $response, array $args) : Response;
 }
