1630fa891e2ecb1d6ff24ff37f0336dd5999516d
Elian <arnaud35u@etu.univ-lorraine.fr>
2023-06-15 21:56:41 +0200

    Correction + config pour docketu

--- gift.appli/src/actions/AbstractAction.php
+++ gift.appli/src/actions/AbstractAction.php
@@ -6,6 +6,5 @@ use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 
 abstract class AbstractAction {
-
     abstract public function __invoke(Request $request, Response $response, array $args) : Response;
 }
