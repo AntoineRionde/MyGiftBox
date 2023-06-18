4b8d61587b221e0b3160d417b5b06cf52411cfff
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2023-06-18 15:46:21 +0200

    profil.twig(à modifier) et fix box.twig

--- gift.appli/src/actions/GetProfile.php
+++ gift.appli/src/actions/GetProfile.php
@@ -4,12 +4,27 @@ namespace gift\app\actions;
 
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
+use Slim\Routing\RouteContext;
+use Slim\Views\Twig;
 
 class GetProfile extends AbstractAction
 {
 
+    public function __construct()
+    {
+        if (session_status() === PHP_SESSION_NONE)
+            session_start();
+    }
     public function __invoke(Request $request, Response $response, array $args): Response
     {
         // TODO: Implement __invoke() method.
+        $basePath = RouteContext::fromRequest($request)->getBasePath() ;
+        $css_dir = $basePath . "/styles";
+        $img_dir = $basePath . "/img";
+        $shared_dir = $basePath . "/shared/img";
+        $resources = ['css' => $css_dir, 'img' => $img_dir, 'shared' => $shared_dir, 'user' => $_SESSION['user'] ?? null];
+        $view = Twig::fromRequest($request);
+        $view->render($response, 'profil.twig', ['resources' => $resources]);
+        return $response;
     }
 }
