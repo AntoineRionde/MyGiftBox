b39663170f3b6bf8b5c44a667038b98087d0c9bc
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2023-06-18 12:01:01 +0200

    Modifs routes api

--- gift.api/src/conf/routes.php
+++ gift.api/src/conf/routes.php
@@ -1,16 +1,16 @@
 <?php
 declare(strict_types=1);
 
-
 use Slim\App;
+use gift\api\actions\getApiBoxActions;
 return function (App $app) {
 
-    $app->get(gift\api\actions\getApiPrestationsActions::class, '/api/prestations')->setName('prestations');
+    $app->get('/api/prestations', gift\api\actions\GetApiPrestationsActions::class)->setName('prestations');
 
-    $app->get('/api/categories', gift\api\actions\getApiCategoriesAction::class)->setName('categories');
+    $app->get('/api/categories', gift\api\actions\GetApiCategoriesAction::class)->setName('categories');
 
-    $app->get('/api/categories/{id}/prestation', gift\api\actions\getApiPrestationByCategorieAction::class)->setName('prestationByCategorie');
+    $app->get('/api/categories/{id}/prestation', gift\api\actions\GetApiPrestationByCategorieAction::class)->setName('prestationByCategorie');
 
-    $app->get('/api/coffrets/{id}', gift\api\actions\getApiBoxActions::class)->setName('boxById');
+    $app->get('/api/coffrets/{id}', GetApiBoxActions::class)->setName('boxById');
 
 };
