3564fb4b23d01b90c008e586a2758a234e2da0d5
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2023-06-18 12:07:21 +0200

    modifs routes.php api

--- gift.api/src/conf/routes.php
+++ gift.api/src/conf/routes.php
@@ -3,13 +3,16 @@ declare(strict_types=1);
 
 use Slim\App;
 use gift\api\actions\getApiBoxActions;
+use gift\api\actions\getApiPrestationsActions;
+use gift\api\actions\getApiCategoriesAction;
+use gift\api\actions\getApiPrestationByCategorieAction;
 return function (App $app) {
 
-    $app->get('/api/prestations', gift\api\actions\GetApiPrestationsActions::class)->setName('prestations');
+    $app->get('/api/prestations', GetApiPrestationsActions::class)->setName('prestations');
 
-    $app->get('/api/categories', gift\api\actions\GetApiCategoriesAction::class)->setName('categories');
+    $app->get('/api/categories', GetApiCategoriesAction::class)->setName('categories');
 
-    $app->get('/api/categories/{id}/prestation', gift\api\actions\GetApiPrestationByCategorieAction::class)->setName('prestationByCategorie');
+    $app->get('/api/categories/{id}/prestation', GetApiPrestationByCategorieAction::class)->setName('prestationsByCategorie');
 
     $app->get('/api/coffrets/{id}', GetApiBoxActions::class)->setName('boxById');
 
