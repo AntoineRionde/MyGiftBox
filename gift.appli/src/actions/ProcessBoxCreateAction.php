eaf8694b58077bdd143141bf5cc83fd7114e6839
Elian <arnaud35u@etu.univ-lorraine.fr>
2023-06-18 14:35:25 +0200

    Optimisation du code

--- gift.appli/src/actions/ProcessBoxCreateAction.php
+++ gift.appli/src/actions/ProcessBoxCreateAction.php
@@ -8,7 +8,7 @@ use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 use Slim\Routing\RouteContext;
 
-class BoxCreateProcessAction extends AbstractAction
+class ProcessBoxCreateAction extends AbstractAction
 {
     public function __construct()
     {
@@ -20,19 +20,17 @@ class BoxCreateProcessAction extends AbstractAction
     {
         $routeContext = RouteContext::fromRequest($request);
         $urlLogin = $routeContext->getRouteParser()->urlFor('login');
-        /*if (!isset($_SESSION['user'])) {
+
+        if (!isset($_SESSION['user'])) {
             return  $response->withHeader('Location', $urlLogin)->withStatus(302);
-        }*/
+        }
 
         $data = $request->getParsedBody();
-        $box['libelle'] = filter_var($data['libelle'], FILTER_SANITIZE_SPECIAL_CHARS);
-        $box['description'] = filter_var($data['description'], FILTER_SANITIZE_SPECIAL_CHARS);
-        $box['kdo'] = $data['kdo'] == 'on' ? 1 : 0;
-        $box['message_kdo'] = filter_var($data['message_kdo'], FILTER_SANITIZE_SPECIAL_CHARS);
         $boxService = new BoxService();
-        $box = $boxService->createEmptyBox($box);
-        $url = $routeContext->getRouteParser()->urlFor('box', ['box_id' => $box['id']]);
+        $box = $boxService->createEmptyBox($data);
+        $_SESSION['box_id'] = $box['id'];
+        $url = $routeContext->getRouteParser()->urlFor('prestations');
 
-        return $response->withHeader('location', $routeContext->urlFor($url))->withStatus(302);
+        return $response->withHeader('location', $routeContext->getRouteParser()->urlFor($url))->withStatus(302);
     }
 }
